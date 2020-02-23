0a6a95069cef9e51b14ee3bece37a7b6f7383c3d
hazelpy <hazelpytv@gmail.com>
2020-02-22 21:30:14 -0500

    Added a clear function for practice and a default message when no arguments are provided.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import sys
+import os
+
+from os import system as System
 
 __readme__ = """\
 This is my first Linux application!
 Using Python 3, I wrote this code myself. \
 """
+
+def Clear():
+    clearname = "clear"
+
+    if os.name == "nt":
+        clearname = "cls"
+
+    System(clearname)
 
 def ReadMe():
     print(__readme__)
@@ -11,8 +22,12 @@ def ReadMe():
 def Version():
     print("Linuxpy v0.0.1")
 
-enum = {"-readme": ReadMe, "-v": Version}
+def _end():
+    print("Usage: python3 main.py [-v | -readme | -clear]")
+    sys.exit(0)
 
-if not len(sys.argv) > 1: sys.exit(0)
+enum = {"-readme": ReadMe, "-v": Version, "-clear": Clear}
+
+if not len(sys.argv) > 1: _end()
 if sys.argv[1] in enum.keys():
     enum[sys.argv[1]]()
