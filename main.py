78c42f4ef6fc93d4c0ce092d5af793c3a422718e
hazelpy <hazelpytv@gmail.com>
2020-02-22 20:43:18 -0500

    Using sys.argv to take in --readme parameter

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
+import sys
 
 __readme__ = """\
 This is my first Linux application!
-Using Python 3, I wrote this code myself.
+Using Python 3, I wrote this code myself. \
 """
-
 
 def ReadMe():
     print(__readme__)
 
-
-ReadMe()
+if not len(sys.argv) > 1: sys.exit(0)
+if sys.argv[1] == "--readme":
+    ReadMe()
