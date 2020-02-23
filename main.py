d9f1c5f9980eaa225ae72d7755b6fffb6487dd43
hazelpy <hazelpytv@gmail.com>
2020-02-22 20:58:55 -0500

    Added -v and renamed --readme to -readme, changed a few things on how sys.argv activates functions

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ Using Python 3, I wrote this code myself. \
 def ReadMe():
     print(__readme__)
 
+def Version():
+    print("Linuxpy v0.0.1")
+
+enum = {"-readme": ReadMe, "-v": Version}
+
 if not len(sys.argv) > 1: sys.exit(0)
-if sys.argv[1] == "--readme":
-    ReadMe()
+if sys.argv[1] in enum.keys():
+    enum[sys.argv[1]]()
